ab168e3d51007598271fe49e88e4ad7b7bc0ea69
berkut126 <berkut126@gmail.com>
2021-01-27 18:25:09 +0300

    Added user authentication.

---
+++ auth/logout.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ *
+ * Copyright (c) 2021 Andrey Ivanov <berkut126@gmail.com>
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+// TODO: change cookie name to loggedIn
+$cookie_name = "logedIn";
+// Reset cookie
+setcookie($cookie_name, 0, 0, "/");
+// See if user was working with the view other than lessons
+if(isset($_GET["location"])){
+    // Redirect user
+    header("Location:http://andrew.pokaza.net/#".$_GET["location"]);
+}
+// Redirect user
+header("Location:http://andrew.pokaza.net/");
+
+?>
