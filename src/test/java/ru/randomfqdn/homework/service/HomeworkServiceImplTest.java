2fa41931d3940e0d5724fa42ef1c15e4a8baba03
berkut126 <berkut126@gmail.com>
2021-02-02 20:58:27 +0300

    Added HomeworkService test.

--- src/test/java/ru/randomfqdn/homework/service/HomeworkServiceImplTest.java
+++ src/test/java/ru/randomfqdn/homework/service/HomeworkServiceImplTest.java
@@ -3,28 +3,66 @@ package ru.randomfqdn.homework.service;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import ru.randomfqdn.homework.configuration.JpaConfiguration;
+import ru.randomfqdn.homework.model.Lesson;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HomeworkServiceImplTest {
 
+    private GenericApplicationContext context;
+    private HomeworkService service;
+
     @BeforeEach
     void setUp() {
+
+        context = new AnnotationConfigApplicationContext(JpaConfiguration.class);
+
+        service = context.getBean(HomeworkService.class);
+        assertNotNull(service);
+
     }
 
     @AfterEach
     void tearDown() {
+
+        context.close();
+
     }
 
     @Test
     void findAll() {
+
+        var homework = service.findAll();
+        assertEquals(4, homework.size());
+
+        homework.forEach(System.out::println);
+
     }
 
     @Test
     void findAllByLesson() {
+
+        var lesson = new Lesson();
+        lesson.setId(1);
+        lesson.setName("Math");
+
+        var homework = service.findAllByLesson(lesson);
+        assertEquals(2, homework.size());
+
+        homework.forEach(System.out::println);
+
     }
 
     @Test
     void findAllById() {
+
+        var homework = service.findById(1);
+        assertEquals("LIM", homework.getTask());
+
+        System.out.println(homework);
+
     }
 }
